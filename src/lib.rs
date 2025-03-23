77274c4bdc9d1032d0507cdd7b822a1475a4500e
SomeB1oody <stanyin64@gmail.com>
2025-03-23 00:34:18 -0700

    add `math` module, put all math functions here

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,27 @@
-/// Calculate the sum of squared errors between two vectors: sum((predicted - actual)^2)
-///
-/// # Parameters
-/// * `predicted` - Predicted values vector (y')
-/// * `actual` - Actual values vector (y)
-///
-/// # Return Value
-/// * Sum of squared errors sum((predicted_i - actual_i)^2)
-///
-/// # Panic
-/// The function will panic if the vectors have different lengths
-pub fn sum_of_squared_errors(predicted: &[f64], actual: &[f64]) -> f64 {
-    // Ensure both vectors have the same length
-    assert_eq!(predicted.len(), actual.len(), "Vectors must have the same length");
-    // Calculate the sum of squared errors
-    predicted.iter()
-        .zip(actual.iter())
-        .map(|(p, a)| {
-            let diff = p - a;
-            diff * diff // Calculate square
-        })
-        .sum()
-}
+pub mod math {
+    /// Calculate the sum of squared errors between two vectors: sum((predicted - actual)^2)
+    ///
+    /// # Parameters
+    /// * `predicted` - Predicted values vector (y')
+    /// * `actual` - Actual values vector (y)
+    ///
+    /// # Return Value
+    /// * Sum of squared errors sum((predicted_i - actual_i)^2)
+    ///
+    /// # Panic
+    /// The function will panic if the vectors have different lengths
+    pub fn sum_of_squared_errors(predicted: &[f64], actual: &[f64]) -> f64 {
+        // Ensure both vectors have the same length
+        assert_eq!(predicted.len(), actual.len(), "Vectors must have the same length");
+        // Calculate the sum of squared errors
+        predicted.iter()
+            .zip(actual.iter())
+            .map(|(p, a)| {
+                let diff = p - a;
+                diff * diff // Calculate square
+            })
+            .sum()
+    }
 
 /// Calculates the Sum of Square Total (SST)
 ///
